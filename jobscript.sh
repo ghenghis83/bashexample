14189eaf48c99c96e73d82023bb67db22d0eb301
ghenghis83 <66876879+ghenghis83@users.noreply.github.com>
2021-03-19 19:18:33 +0100

    Create jobscript.sh

---
+++ jobscript.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "bienvenido al planeta $planteta"
+if [ "$agente" = "true" ]
+then 
+	echo "preparense para la misision agente x"
+else
+	echo "miau señor $nombre, a morrir!"
+fi
+echo "..."
+sleep 10
+echo "suerte perro"
